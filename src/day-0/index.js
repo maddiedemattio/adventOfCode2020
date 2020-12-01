a56e0dc7f41f15b9c1a4149a07658734d3cd8291
Maddie DeMattio <maddiedemattio@maddies-air.home>
2020-11-30 22:37:46 -0500

    updated .gitignore & filepath

--- src/day-0/index.js
+++ src/day-0/index.js
@@ -1,14 +1,13 @@
 import filereader from '../utils/filereader.js';
 import { constants } from 'fs';
 
-const inputArray = filereader('input.txt');
+const inputArray = filereader('src/day-0/input.txt');
 
 // This is the day 1 of the 2019 set. Just doing it to get my environment set up!
 const solve = () => {
     // TODO: add your solution here
     let totalSum = 0;
     inputArray.map((num) => {
-        console.log(totalSum);
         totalSum += Math.floor(num / 3) - 2;
     });
 
